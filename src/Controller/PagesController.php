b7e2cf75ff91522e058807a695088191a52d7a6c
Issamhaji <issamhaji606@gmail.com>
2023-01-24 16:49:49 +0100

    add maker bundel and twig ,add php.ini config , setup PagesController and HomePage

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {   
+        #!!#
+        dump(date_default_timezone_get());
+        return $this->render("pages/index.html.twig");
+    }
+}
